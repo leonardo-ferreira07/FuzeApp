bb6c1cbe24600fbdb80225014746d06c565a3adf
Leonardo Kaminski Ferreira <leonardo.kaminski.ferreira@gmail.com>
2023-03-02 15:36:45 -0300

    [FEAT] Add Match model

---
+++ Fuze/Models/Match.swift
@@ -0,0 +1,58 @@
+//
+//  Match.swift
+//  Fuze
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 02/03/23.
+//
+
+import Foundation
+
+struct Match: Decodable {
+    let beginAt: String?
+    let opponents: [OpponentMain]?
+    let league: League?
+    let serie: Serie?
+    
+    private enum CodingKeys: String, CodingKey {
+        case beginAt = "begin_at"
+        case opponents
+        case league
+        case serie
+    }
+}
+
+// MARK: Inner objects of a Match
+
+struct OpponentMain: Decodable {
+    let opponent: Opponent?
+}
+
+struct Opponent: Decodable {
+    let acronym: String?
+    let name: String?
+    let imageUrl: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case acronym
+        case name
+        case imageUrl = "image_url"
+    }
+}
+
+struct League: Decodable {
+    let name: String?
+    let imageUrl: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case imageUrl = "image_url"
+    }
+}
+
+struct Serie: Decodable {
+    let fullName: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+    }
+}
